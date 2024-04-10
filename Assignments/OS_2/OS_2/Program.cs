7bbff58980d6c5c0b20491e82471436a7725cd32
Omid Tarabavar <omidtarab1300@gmail.com>
2024-04-11 01:45:04 +0330

    Add assignment 2

---
+++ Assignments/OS_2/OS_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+
+namespace OS_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Waiting for flash device...");
+            while (true)
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                foreach (DriveInfo drive in drives)
+                {
+                    if (drive.DriveType == DriveType.Removable)
+                    {
+                        System.Diagnostics.Process.Start("mspaint.exe");
+                        return;
+                    }
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
