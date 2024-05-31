ec3f74906b7297348679bdf828429330dca5dd1d
Omid Tarabavar <omidtarab1300@gmail.com>
2024-06-01 03:10:55 +0330

    Remove unwanted files

--- Project/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProjectLMS
-{
-    internal class Student : User
-    {
-        int type = 2;
-        public Student(int userId, string name, string email, string password) : base(userId, name, email, password, 2)
-        { }
-    }
-}
